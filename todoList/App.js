a354f31764c937eec251b9266dd76d37ed5ee6a8
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-07-04 22:56:40 -0500

    Update App.js

--- todoList/App.js
+++ todoList/App.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <Text>This is my page</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
